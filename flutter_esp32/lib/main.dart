cf5f5822af5d468425a71ac762fcf58b494d9873
Simon Radosavljević <simon.fhz@gmail.com>
2024-12-28 20:44:39 +0100

    Location works, but in gallery we cant see it. Stream still doesnt work

--- flutter_esp32/lib/main.dart
+++ flutter_esp32/lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:typed_data'; // Rad sa bajtovima
+import 'dart:io'; // Provera konekcije
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:photo_manager/photo_manager.dart'; // Rad sa galerijom
-import 'package:flutter_vlc_player/flutter_vlc_player.dart'; // VLC Player
-import 'dart:io'; // Provera konekcije
+import 'package:location/location.dart' as loc; // GPS lokacija
+import 'package:image/image.dart' as img; // Rad sa slikama i EXIF metapodacima
+import 'package:flutter_vlc_player/flutter_vlc_player.dart'; // VLC Player za stream
+import 'package:exif/exif.dart'; // EXIF biblioteka za metapodatke
 
 void main() {
   runApp(const MyApp());
@@ -63,6 +66,9 @@ class _CameraScreenState extends State<CameraScreen> {
   // VLC Player Controller
   late VlcPlayerController _vlcController;
 
+  // Lokacija
+  loc.Location location = loc.Location();
+
   @override
   void initState() {
     super.initState();
@@ -78,7 +84,6 @@ class _CameraScreenState extends State<CameraScreen> {
       options: VlcPlayerOptions(),
     );
 
-    // Dodajemo osluškivanje stanja inicijalizacije
     _vlcController.addListener(() {
       if (_vlcController.value.isInitialized) {
         print("VLC je uspešno inicijalizovan.");
@@ -94,7 +99,7 @@ class _CameraScreenState extends State<CameraScreen> {
     super.dispose();
   }
 
-  // Funkcija za proveru mrežne konekcije
+  // Provera mrežne konekcije
   void testConnection() async {
     try {
       final result = await InternetAddress.lookup('192.168.0.7');
@@ -106,7 +111,36 @@ class _CameraScreenState extends State<CameraScreen> {
     }
   }
 
-  // Funkcija za hvatanje slike
+  // Funkcija za proveru dozvola za GPS
+  Future<bool> _checkPermissions() async {
+    bool serviceEnabled;
+    loc.PermissionStatus permissionGranted;
+
+    // Proveri da li je GPS omogućen
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        print('GPS nije omogućen!');
+        return false;
+      }
+    }
+
+    // Proveri dozvole
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == loc.PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != loc.PermissionStatus.granted) {
+        print('GPS dozvola nije odobrena!');
+        return false;
+      }
+    }
+
+    print('Dozvola za GPS odobrena.');
+    return true;
+  }
+
+  // Hvatanje slike
   Future<void> _captureStillImage() async {
     setState(() {
       _isLoading = true;
@@ -131,95 +165,84 @@ class _CameraScreenState extends State<CameraScreen> {
     }
   }
 
-  // Funkcija za čuvanje slike
-  Future<void> _saveImage(Uint8List imageBytes) async {
-    String filename = 'moja_slika_${DateTime.now().millisecondsSinceEpoch}.jpg';
+  // Čuvanje slike sa GPS lokacijom
+  Future<void> _saveImageWithLocation(Uint8List imageBytes) async {
+    try {
+      print("Početak čuvanja slike...");
 
-    final result = await PhotoManager.editor.saveImage(imageBytes, filename: filename);
-    if (result != null) {
-      print('Slika uspešno sačuvana!');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Slika je sačuvana u galeriji!'), duration: Duration(seconds: 2)),
+      // Provera dozvola
+      bool permissionsOk = await _checkPermissions();
+      if (!permissionsOk) return;
+
+      // 1. Preuzimanje GPS lokacije
+      final loc.LocationData locationData = await location.getLocation();
+      final double latitude = locationData.latitude ?? 0.0;
+      final double longitude = locationData.longitude ?? 0.0;
+
+      print('GPS lokacija: $latitude, $longitude');
+
+      // 2. Učitavanje slike
+      final img.Image? originalImage = img.decodeImage(imageBytes);
+      if (originalImage == null) {
+        print('Greška pri učitavanju slike.');
+        return;
+      }
+
+      // 3. Kodiranje slike
+      final Uint8List encodedImage = Uint8List.fromList(img.encodeJpg(originalImage));
+
+      // 4. Snimanje slike u galeriju
+      final AssetEntity? result = await PhotoManager.editor.saveImage(
+        encodedImage,
+        filename: 'moja_slika_${DateTime.now().millisecondsSinceEpoch}.jpg',
       );
-    } else {
-      print('Greška pri snimanju slike!');
+
+      if (result != null) {
+        print('Slika sa GPS lokacijom sačuvana!');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Slika sa lokacijom je sačuvana!')),
+        );
+      } else {
+        print('Greška pri snimanju slike!');
+      }
+    } catch (e) {
+      print('Greška tokom čuvanja slike: $e');
     }
   }
 
   // Start/stop stream funkcija
   void _startStopStream() async {
     setState(() {
-      _isStreaming = !_isStreaming; // Menja status stream-a
+      _isStreaming = !_isStreaming;
     });
 
     if (_isStreaming) {
-      print('Status inicijalizacije: ${_vlcController.value.isInitialized}');
-      print('Trenutni status kontrole: ${_vlcController.value}'); 
       if (_vlcController.value.isInitialized) {
-        _vlcController.play(); // Pokreće stream
+        _vlcController.play();
       } else {
-        print('VLC još uvek nije inicijalizovan. Pokušaj ponovo.');
         setState(() {
-          _isStreaming = false; // Vraća status
+          _isStreaming = false;
         });
       }
     } else {
-      _vlcController.stop(); // Zaustavlja stream
+      _vlcController.stop();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ESP32 Camera Control'),
-      ),
+      appBar: AppBar(title: const Text('ESP32 Camera Control')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Prikaz slike
-            if (_imageBytes != null) ...[
-              Image.memory(_imageBytes!),
-              const SizedBox(height: 20),
-            ],
-
-            // Prikaz učitavanja
-            if (_isLoading) ...[
-              const CircularProgressIndicator(),
-              const SizedBox(height: 20),
-            ],
-
-            // Dugme za hvatanje slike
-            ElevatedButton(
-              onPressed: _captureStillImage,
-              child: const Text('Get Still'),
-            ),
-
-            // Dugme za čuvanje slike
-            if (_imageCaptured) ...[
-              ElevatedButton(
-                onPressed: () => _saveImage(_imageBytes!),
-                child: const Text('Save Image'),
-              ),
-            ],
-
-            // Prikaz Live Stream-a
-            if (_isStreaming) ...[
-              Container(
-                height: 300,
-                child: VlcPlayer(
-                  controller: _vlcController,
-                  aspectRatio: 16 / 9,
-                  placeholder: const Center(child: CircularProgressIndicator()),
-                ),
-              ),
-            ],
-
-            // Dugme za start/stop stream-a
-            ElevatedButton(
-              onPressed: _startStopStream,
-              child: Text(_isStreaming ? 'Stop Stream' : 'Start Stream'),
+            if (_imageBytes != null) Image.memory(_imageBytes!),
+            if (_isLoading) const CircularProgressIndicator(),
+            ElevatedButton(onPressed: _captureStillImage, child: const Text('Get Still')),
+            if (_imageCaptured) ElevatedButton(
+              onPressed: () => _saveImageWithLocation(_imageBytes!),
+              child: const Text('Save Image'),
             ),
           ],
         ),
--- flutter_esp32/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_esp32/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import flutter_local_notifications
 import geolocator_apple
+import location
 import path_provider_foundation
 import photo_manager
 import video_player_avfoundation
@@ -14,6 +15,7 @@ import video_player_avfoundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
