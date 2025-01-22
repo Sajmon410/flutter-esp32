f00115c8f6b71fe100e148a2b73194789c9aaf54
Simon Radosavljević <simon.fhz@gmail.com>
2025-01-22 18:26:38 +0100

    MDNS server esp32.local added.

--- Camera Server/PoC_CameraWebServer/PoC_CameraWebServer.ino
+++ Camera Server/PoC_CameraWebServer/PoC_CameraWebServer.ino
@@ -1,118 +1,129 @@
 #include "esp_camera.h"
-#include <WiFi.h>
-
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-
-// Select camera model
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-
-// https://arduino.stackexchange.com/questions/70839/esp32-cam-failed-to-get-frame-in-time
-// https://gitcode.net/mirrors/espressif/esp32-camera
-#include "camera_pins.h"
-
-const char* ssid = "SimoniPhone";
-const char* pass = "simon001";
-
-
-
-void startCameraServer();
-
-void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.printf("ssid: %s\r\n", ssid);
-  Serial.printf("pass: %s\r\n", pass);
-
-
-int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-      // Ispiši redni broj mreže i njen SSID (naziv)
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.println(WiFi.SSID(i));
-      // Opcionalno možeš ispisati i jačinu signala (RSSI)
-      Serial.print("Jačina signala (RSSI): ");
-      Serial.println(WiFi.RSSI(i));
-      Serial.println("-----------------------");
-      }
-    }
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("\r\nWiFi connected");
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 16000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 10;
-
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-  sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-
-
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(10000);
-}
+ #include <WiFi.h>
+ #include <ESPmDNS.h>
+ //
+ // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+ // Ensure ESP32 Wrover Module or other board with PSRAM is selected
+ // Partial images will be transmitted if image exceeds buffer size
+ //
+ 
+ // Select camera model
+ #define CAMERA_MODEL_AI_THINKER // Has PSRAM
+ 
+ // https://arduino.stackexchange.com/questions/70839/esp32-cam-failed-to-get-frame-in-time
+ // https://gitcode.net/mirrors/espressif/esp32-camera
+ #include "camera_pins.h"
+ 
+ const char* ssid = "SimoniPhone";
+ const char* pass = "simon001";
+ 
+ 
+ 
+ void startCameraServer();
+ 
+ void setup() {
+ 
+ Serial.begin(115200);
+ Serial.setDebugOutput(true);
+ Serial.printf("ssid: %s\r\n", ssid);
+ Serial.printf("pass: %s\r\n", pass);
+ 
+ 
+ int n = WiFi.scanNetworks();
+ Serial.println("scan done");
+ if (n == 0) {
+ Serial.println("no networks found");
+ } else {
+ Serial.print(n);
+ Serial.println(" networks found");
+ for (int i = 0; i < n; ++i) {
+ // Ispiši redni broj mreže i njen SSID (naziv)
+ Serial.print(i + 1);
+ Serial.print(": ");
+ Serial.println(WiFi.SSID(i));
+ // Opcionalno možeš ispisati i jačinu signala (RSSI)
+ Serial.print("Jačina signala (RSSI): ");
+ Serial.println(WiFi.RSSI(i));
+ Serial.println("-----------------------");
+ }
+ }
+ WiFi.begin(ssid, pass);
+ 
+ while (WiFi.status() != WL_CONNECTED) 
+ {
+ Serial.print(".");
+ delay(1000);
+ }
+ Serial.println("\r\nWiFi connected");
+ 
+ //mDNS
+ if(!MDNS.begin("esp32")){ //hostname "esp32.local"
+ Serial.println("Error setting up MDNS responder!");
+ while(1){
+ delay(1000);
+ }
+ }
+ Serial.println("MDNS server started.");
+ 
+ camera_config_t config;
+ config.ledc_channel = LEDC_CHANNEL_0;
+ config.ledc_timer = LEDC_TIMER_0;
+ config.pin_d0 = Y2_GPIO_NUM;
+ config.pin_d1 = Y3_GPIO_NUM;
+ config.pin_d2 = Y4_GPIO_NUM;
+ config.pin_d3 = Y5_GPIO_NUM;
+ config.pin_d4 = Y6_GPIO_NUM;
+ config.pin_d5 = Y7_GPIO_NUM;
+ config.pin_d6 = Y8_GPIO_NUM;
+ config.pin_d7 = Y9_GPIO_NUM;
+ config.pin_xclk = XCLK_GPIO_NUM;
+ config.pin_pclk = PCLK_GPIO_NUM;
+ config.pin_vsync = VSYNC_GPIO_NUM;
+ config.pin_href = HREF_GPIO_NUM;
+ config.pin_sscb_sda = SIOD_GPIO_NUM;
+ config.pin_sscb_scl = SIOC_GPIO_NUM;
+ config.pin_pwdn = PWDN_GPIO_NUM;
+ config.pin_reset = RESET_GPIO_NUM;
+ config.xclk_freq_hz = 16000000;
+ config.pixel_format = PIXFORMAT_JPEG;
+ 
+ config.frame_size = FRAMESIZE_SVGA;
+ config.jpeg_quality = 12;
+ config.fb_count = 10;
+ 
+ 
+ // camera init
+ esp_err_t err = esp_camera_init(&config);
+ if (err != ESP_OK) {
+ Serial.printf("Camera init failed with error 0x%x", err);
+ return;
+ }
+ 
+ sensor_t * s = esp_camera_sensor_get();
+ // initial sensors are flipped vertically and colors are a bit saturated
+ if (s->id.PID == OV3660_PID) {
+ s->set_vflip(s, 1); // flip it back
+ s->set_brightness(s, 1); // up the brightness just a bit
+ s->set_saturation(s, -2); // lower the saturation
+ }
+ // drop down frame size for higher initial frame rate
+ s->set_framesize(s, FRAMESIZE_QVGA);
+ 
+ #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+ s->set_vflip(s, 1);
+ s->set_hmirror(s, 1);
+ #endif
+ 
+ 
+ 
+ startCameraServer();
+ 
+ Serial.print("Camera Ready! Use 'http://");
+ Serial.print(WiFi.localIP());
+ Serial.println("' to connect");
+ }
+ 
+ void loop() {
+ // put your main code here, to run repeatedly:
+ delay(10000);
+ }
